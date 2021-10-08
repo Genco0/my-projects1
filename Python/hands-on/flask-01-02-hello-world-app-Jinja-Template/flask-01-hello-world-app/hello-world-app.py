e2b43290ba25cb691f9a3af5f98e8be388b83913
mehmet genc <plt.mehmetgenc@gmail.com>
2021-10-08 22:22:23 +0300

    flask-01-02 added

---
+++ Python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+app = Flask(__name__) # obje oluşturduk
+
+@app.route('/')  # ilk karşımıza çıkacak sayfayı düzenliyoruz
+def hello():
+    return "Hello world from Flask!!!"
+
+@app.route('/second')
+def second():
+    return "bize her yer ankara!!!" # searcha localhost:2000/second yazınca browserde ankarayı göreceğiz
+
+@app.route('/third/subthird')
+def third():
+    return 'This is the subpage of third page'
+
+@app.route('/fourt/<string:id>')
+def forth(id) :
+    return f"Id number of this page {id}"
+
+
+
+
+
+
+
+if __name__ == '__main__' :
+    app.run(debug=True, port=2000) # flask çatısı bu şekilde localhost:2000 de çalışacak
